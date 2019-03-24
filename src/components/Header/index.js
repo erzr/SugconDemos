517a28efd84517606581cd412b3b9e9c0d74e4a4
erzr <aelamarre@gmail.com>
2019-03-24 13:56:34 -0400

    integrated query example, refactored header.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,39 +1,37 @@
 import React from 'react';
 import { loader as gqlLoader } from 'graphql.macro';
-import { Query } from "react-apollo";
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { SITE_SETTINGS_ID } from '../../constants';
+import GraphQLData from '../../lib/GraphQLData';
 import { withNamespaces } from 'react-i18next';
 
 const ConnectedDemoQuery = gqlLoader('./query.graphql');
 
-let DemoNavDropdown = ({demoLinks}) => (
+let DemoNavDropdown = ({ demoLinks }) => (
     <NavDropdown title="Demos" id="basic-nav-dropdown">
         {demoLinks.map((link, i) => <NavDropdown.Item href={link.url} key={i}>{link.pageTitle.value}</NavDropdown.Item>)}
     </NavDropdown>
 );
 
-let Header = ({ t, i18n }) => (
-    <Query query={ConnectedDemoQuery} variables={ { id: SITE_SETTINGS_ID } }>
-        {({ loading, error, data }) => {
-            if (loading) return '';
-            if (error) return `Error! ${error.message}`;
-            return (
-                <Navbar bg="dark" expand="lg" variant="dark">
-                    <Container>
-                        <Navbar.Brand href="/">{data.item.siteTitle.value}</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="mr-auto">
-                                <Nav.Link href="/">{t('Home')}</Nav.Link>
-                                {data.item.demoLinks && <DemoNavDropdown demoLinks={data.item.demoLinks.targetItems} />}
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar >
-            );
-        }}
-    </Query>
-);
+let Header = ({t, headerQuery}) => {
+    const { item } = headerQuery;
 
-export default withNamespaces()(Header);
+    return (
+        <Navbar bg="dark" expand="lg" variant="dark">
+            <Container>
+                <Navbar.Brand href="/">{item && item.siteTitle.value}</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        {<Nav.Link href="/">{t('Home')}</Nav.Link>}
+                        {item && item.demoLinks && <DemoNavDropdown demoLinks={item.demoLinks.targetItems} />}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+const HeaderWithNamespaces = withNamespaces()(Header);
+
+export default GraphQLData(ConnectedDemoQuery, { name: 'headerQuery', options: { variables: { id: SITE_SETTINGS_ID } } })(HeaderWithNamespaces);
---
+++ src/components/IntegratedDemo/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from '@sitecore-jss/sitecore-jss-react';
+
+const IntegratedDemo = (props) => (
+  <div>
+    <p>IntegratedDemo Component</p>
+  </div>
+);
+
+export default IntegratedDemo;
